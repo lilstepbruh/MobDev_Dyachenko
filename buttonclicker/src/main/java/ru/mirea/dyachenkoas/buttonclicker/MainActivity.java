eb270903e581fedba3f7b0ef71c6097867880514
Andrew <anko.mywork@protonmail.com>
2024-03-09 03:09:02 +0300

    first commit

---
+++ buttonclicker/src/main/java/ru/mirea/dyachenkoas/buttonclicker/MainActivity.java
@@ -0,0 +1,47 @@
+package ru.mirea.dyachenkoas.buttonclicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvOut;
+    private CheckBox checkBox;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvOut = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        checkBox = findViewById(R.id.checkBox);
+
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Мой номер по списку № 11");
+                checkBox.toggle();
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+
+
+
+    }
+    public void onMyButtonClick(View v){
+        tvOut.setText("Это не я сделал");
+        checkBox.toggle();
+    }
+}
